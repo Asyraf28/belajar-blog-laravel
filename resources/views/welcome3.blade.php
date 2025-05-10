11b0274d876633d127f30437cdcccfe051cca0b6
Asyraf28 <mohasyraf28@gmail.com>
2025-05-10 20:44:43 +0700

    Conflict solve

---
+++ resources/views/welcome3.blade.php
@@ -0,0 +1,7 @@
+<?php
+$judul = "Ini  Betul Title";
+?>
+<x-halaman-layout :title="$judul">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora totam quas fugiat non. Voluptatem recusandae quaerat porro adipisci explicabo eligendi blanditiis sed consequatur obcaecati!</p>
+    <x-slot name="tanggal">17 Agustus 1945</x-slot>
+</x-halaman-layout>
